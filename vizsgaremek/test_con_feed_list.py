749c9bb4a38f0675a0e7d580eb577263aff6f1ae
Attila Boros <boros.attilla@gmail.com>
2021-08-15 14:47:37 +0200

    ready

--- vizsgaremek/test_con_feed_list.py
+++ vizsgaremek/test_con_feed_list.py
@@ -1,38 +1,34 @@
 def test_con_feed_list():
-    import time
     from selenium import webdriver
+    import time
+
     from selenium.webdriver.chrome.options import Options
     from webdriver_manager.chrome import ChromeDriverManager
-    from selenium.common.exceptions import NoSuchElementException
 
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
+    opt = Options()
+    opt.headless = True
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=opt)
 
-    # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)  # headless
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    try:
-        driver.get('http://localhost:1667/#/login')
+    driver.get('http://localhost:1667/#/login')
+    # bejelentkezes
+    driver.find_element_by_xpath('//fieldset//input[@placeholder="Email"]').send_keys('testuser1@example.com')
+    driver.find_element_by_xpath('//fieldset//input[@placeholder="Password"]').send_keys('Abcd123$')
+    driver.find_element_by_xpath('//form/button').click()
+    time.sleep(2)
 
-        driver.find_element_by_xpath('//fieldset//input[@placeholder="Email"]').send_keys('testuser1@example.com')
-        driver.find_element_by_xpath('//fieldset//input[@placeholder="Password"]').send_keys('Abcd123$')
-        driver.find_element_by_xpath('//form/button').click()
-        time.sleep(2)
+    driver.find_element_by_xpath('//div[@class="container"]//ul/li[4]/a').click()
 
-        driver.find_element_by_xpath('//div[@class="container"]//ul/li[4]/a').click()
+    time.sleep(3)
+    # cikkek cimeinek kigyujtese
+    my_articles = driver.find_elements_by_xpath('//div[@class="article-preview"]//h1')
 
-        time.sleep(3)
+    list_of_feed = []
+    for row in my_articles:
+        list_of_feed.append(row.text + '\n')
+    print(list_of_feed)
+    # talalatok fileba mentese
+    with open('list_of_feed.txt', 'a') as x:
+        for i in list_of_feed:
+            x.write(i)
 
-        my_articles = driver.find_elements_by_xpath('//div[@class="article-preview"]//h1')
-
-        list_of_feed = []
-        for row in my_articles:
-            list_of_feed.append(row.text + '\n')
-        print(list_of_feed)
-
-        with open('list_of_feed.txt', 'a') as x:
-            for i in list_of_feed:
-                x.write(i)
-
-    finally:
-        driver.close()
+    driver.close()
